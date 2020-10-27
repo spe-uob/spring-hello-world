13ae8872af495ccbfdb092a2bf599202d79202be
Daniel Schien <dschien@gmail.com>
2020-10-27 16:25:02 +0000

    init

---
+++ src/main/java/ac/uk/bristol/cs/spe/livedemo/LiveDemoApplication.java
@@ -0,0 +1,13 @@
+package ac.uk.bristol.cs.spe.livedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveDemoApplication.class, args);
+	}
+
+}
