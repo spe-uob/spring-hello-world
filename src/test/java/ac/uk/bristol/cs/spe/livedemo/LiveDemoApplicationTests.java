2a179a07937e6e7d5ae39bb9ceb3888604ac9d51
Daniel Schien <dschien@gmail.com>
2020-12-14 10:03:14 +0000

    add gitignore

---
+++ src/test/java/ac/uk/bristol/cs/spe/livedemo/LiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.uk.bristol.cs.spe.livedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
