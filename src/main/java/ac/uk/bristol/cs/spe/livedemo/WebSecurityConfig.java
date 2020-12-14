7efc1ef7aead5af6954363f3c2afa7cde37c9d15
Daniel Schien <dschien@gmail.com>
2020-12-14 12:44:50 +0000

    with http basic auth

--- src/main/java/ac/uk/bristol/cs/spe/livedemo/WebSecurityConfig.java
+++ src/main/java/ac/uk/bristol/cs/spe/livedemo/WebSecurityConfig.java
@@ -1,16 +1,28 @@
 package ac.uk.bristol.cs.spe.livedemo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
-                ;
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
